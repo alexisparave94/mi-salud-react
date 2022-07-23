1b330159ebe588267d11ad1e363fac9aa0814d7f
Alexis Parave <aparavev@gmail.com>
2022-07-23 18:57:37 -0500

    STYLES: add main wrapper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import styled from "@emotion/styled";
+import Form from "./components/form";
+import Input from "./components/input";
+import Select from "./components/input_select";
+
+const Title = styled.h1`
+  font-family: 'MONSERRAT', sans-serif;
+  text-align: center;
+  padding: 2rem 0;
+`
+
+const MainWrapper = styled.div`
+  width: 50%;
+  min-width: 22.5rem;
+  margin: auto;
+` 
+
+
+
 function App() {
   return (
-    <h1>Mi Salud</h1>
+    <>
+      <MainWrapper>
+        <Title>MY HEALTH</Title>
+        <Form/>
+      </MainWrapper>
+    </>
   );
 }
 
