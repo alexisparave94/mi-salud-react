4f42e0e62f56ce8076cff8d6364e381b45dd4181
Alexis Parave <aparavev@gmail.com>
2022-07-23 18:45:04 -0500

    STYLES: create styled select

---
+++ src/components/input_select.js
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+  font-family: 'Inter', sans-serif;
+  text-transform: uppercase;
+  font-size: 0.9rem;
+`;
+
+const StyledSelect = styled.select`
+  padding: 0.5rem;
+  border-radius: .5rem;
+  border: 1px solid #D3D3D3;
+  background-color: white;
+  color: #17171C;
+  font-family: 'Inter', sans-serif;
+`;
+
+const SelectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  width: 100%;
+`;
+
+export default function Select({ label }){
+  return (
+    <SelectContainer>
+      <Label>{label}</Label>
+      <StyledSelect name="laboratory">
+        <option value="" selectec>-- Select a laboratory --</option>
+        <option value="Genfar">Genfar</option>
+        <option value="GSK">GSK</option>
+        <option value="Hersil">Hersil</option>
+        <option value="FarmaIndustria">FarmaIndustria</option>
+      </StyledSelect>
+    </SelectContainer>
+  )
+}
