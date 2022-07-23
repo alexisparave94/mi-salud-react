cdf736e4a72419db7c23b31abf140059eb42d703
Alexis Parave <aparavev@gmail.com>
2022-07-23 17:52:30 -0500

    FEAT: add reset styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
 import App from './App';
+import { reset } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={reset}/>
     <App />
   </React.StrictMode>
 );
